57e2c8e4d5dd867802d35e14bafb36475df92a04
glennc <glennc@microsoft.com>
2015-09-02 23:49:44 -0700

    change path

--- globalinstall.ps1
+++ globalinstall.ps1
@@ -10,3 +10,7 @@ Write-Host "Downloading DNVM.ps1 to $dnvmPs1Path"
 $webClient.DownloadFile('https://raw.githubusercontent.com/aspnet/dnvm/brecon/install_g/src/dnvm.ps1', $dnvmPs1Path)
 Write-Host "Downloading DNVM.cmd to $dnvmCmdPath"
 $webClient.DownloadFile('https://raw.githubusercontent.com/aspnet/dnvm/brecon/install_g/src/dnvm.cmd', $dnvmCmdPath)
+
+#$env:PATH = "$env:PATH;C:\ProgramData\Microsoft DNX\bin"
+$oldPath = 	[Environment]::GetEnvironmentVariable("PATH", "MACHINE")
+[Environment]::SetEnvironmentVariable("PATH", "$oldPath;C:\ProgramData\Microsoft DNX\bin", "MACHINE")
