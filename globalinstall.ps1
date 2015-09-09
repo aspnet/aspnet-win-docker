0e3c4cfd93e72d9a301c8f7476e90e66078ad1fa
Glenn Condron <glennc@microsoft.com>
2015-09-09 11:45:49 -0700

    Update globalinstall.ps1

--- globalinstall.ps1
+++ globalinstall.ps1
@@ -7,9 +7,9 @@ if (!(Test-Path $tempPath)) { md $tempPath | Out-Null }
 
 $webClient = New-Object System.Net.WebClient
 Write-Host "Downloading DNVM.ps1 to $dnvmPs1Path"
-$webClient.DownloadFile('https://raw.githubusercontent.com/aspnet/dnvm/brecon/install_g/src/dnvm.ps1', $dnvmPs1Path)
+$webClient.DownloadFile('https://raw.githubusercontent.com/aspnet/Home/dev/dnvm.ps1', $dnvmPs1Path)
 Write-Host "Downloading DNVM.cmd to $dnvmCmdPath"
-$webClient.DownloadFile('https://raw.githubusercontent.com/aspnet/dnvm/brecon/install_g/src/dnvm.cmd', $dnvmCmdPath)
+$webClient.DownloadFile('https://raw.githubusercontent.com/aspnet/Home/dev/dnvm.cmd', $dnvmCmdPath)
 
 #$env:PATH = "$env:PATH;C:\ProgramData\Microsoft DNX\bin"
 $oldPath = 	[Environment]::GetEnvironmentVariable("PATH", "MACHINE")
